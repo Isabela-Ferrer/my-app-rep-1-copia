4f147f3e7a1a19e1d74442e8da1f6d5e1123ce13
Isabela Ferrer <isabelaferrergu@gmail.com>
2023-12-15 16:46:39 -0500

    shadow for title

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="mx-auto bottom-0 pl-2 pb-2 header ">
-        <h1 className="display-1 text-lg-lg text-md-md text-sm-sm text-start" style={{fontSize: "100px", color: "white"}}>A new approach to literacy instruction</h1>
+        <h1 className="display-1 text-lg-lg text-md-md text-sm-sm text-start" style={{fontSize: "100px", color: "white", textShadow: "0px 0px 3px black"}}>A new approach to literacy instruction</h1>
         <h5 className="text-start display-5" style={{color: "white", fontSize: "50p"}}>Making learning a social activity</h5>
     </div>
   );
